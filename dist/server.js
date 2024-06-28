70a31018cbe5fff9afe5b07c9814abb184d5a4c8
Yoana <yoanabarbie@icloud.com>
2024-06-28 12:14:50 +0200

    feat:added middleware and Authors

--- dist/server.js
+++ dist/server.js
@@ -5,9 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+const PORT = process.env.port || 4000;
 app.get('/healthy', (req, res) => {
     res.send('Server is healthy');
 });
-app.listen(4000, () => {
+app.get('/books', (req, res) => {
+    res.send('GET ALL BOOKS');
+});
+app.get('/books', (req, res) => {
+    res.send('Book create');
+});
+app.listen(PORT, () => {
     console.log('Server is running.');
 });
--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,44 @@ import express from 'express';
 
 const app = express();
 
-const PORT = process.env.port || 4000
+//middleware
 
+app.use(express.json())
+
+const PORT = process.env.port || 4000;
 
 app.get('/healthy', (req, res) => {
    res.send('Server is healthy')
 })
 
+//AUTHORS
+
+app.post('/authors', (req, res) => {
+    //recuperar la informacion de la req  
+   console.log(req.body)
+
+   res.send('CREATE AUTHOR')
+})
+
+
+//BOOKS
+
+app.get('/books', (req, res) => {
+    res.send('GET ALL BOOKS')
+ })
+
+ app.post('/books', (req, res) => {
+    res.send('Book CREATED')
+ })
+
+ app.put('/books', (req, res) => {
+    res.send('Book UPDATED')
+ })
+
+ app.delete('/books', (req, res) => {
+    res.send('Book DELETED')
+ })
+
 app.listen(PORT, () => {
     console.log('Server is running.');
 })
-
