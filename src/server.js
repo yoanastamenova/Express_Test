da6b70a15ac07c5806e8ac66a640e56c5c1ea71b
Yoana <yoanabarbie@icloud.com>
2024-06-28 10:23:09 +0200

    kickoff server project

---
+++ src/server.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var app = (0, express_1.default)();
+app.listen(4000, function () {
+    console.log('Server is running.');
+});
