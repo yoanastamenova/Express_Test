a0c37b3a0aa86949849b23ff09fcb532dead778e
Yoana <yoanabarbie@icloud.com>
2024-06-28 11:08:15 +0200

    feat: added const PORT

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
  3. npm i express
  4. npm i --save-dev @types/express
  5. npm i typescript -d
- 6. npm tsc 
+ 6. npm tsc (para compilar)
+ 7. npm i nodemon -D 
+ 8. npm i ts-node -D
+ 
--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,14 @@ import express from 'express';
 
 const app = express();
 
+const PORT = process.env.port || 4000
+
+
 app.get('/healthy', (req, res) => {
    res.send('Server is healthy')
 })
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
     console.log('Server is running.');
 })
 
